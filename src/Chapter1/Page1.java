4bda25d85c85ec549a9bfadb7e561de062b27b47
Abdul Mahmoodi <ahadmahmoodi2@gmail.com>
2023-01-09 09:59:36 -0500

    sentence 2 created

--- src/Chapter1/Page1.java
+++ src/Chapter1/Page1.java
@@ -5,6 +5,7 @@ public class Page1 {
     public static void main(String[] args) {
 
         System.out.println("Avatar slept in the iceland for one hundred years.");
+        System.out.println("Katara and his brother fund Avatar in the iceland.");
 
 
     }
