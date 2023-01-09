1e9c00dcd43993fa3cb075c14e79c42613908ba4
Abdul Mahmoodi <ahadmahmoodi2@gmail.com>
2023-01-09 09:56:45 -0500

    sentence 1 created

--- src/Chapter1/Page1.java
+++ src/Chapter1/Page1.java
@@ -1,4 +1,11 @@
 package Chapter1;
 
 public class Page1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Avatar slept in the iceland for one hundred years.");
+
+
+    }
 }
